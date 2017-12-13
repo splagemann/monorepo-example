a4f25fc4c6a9272d34ea3e757eee318f2df880bb
Sebastian <sebastianplagemann@gmail.com>
2017-12-13 10:29:40 +0100

    Fix elixir version

--- first-elixir-app/mix.exs
+++ first-elixir-app/mix.exs
@@ -5,7 +5,7 @@ defmodule FirstElixirApp.MixProject do
     [
       app: :first_elixir_app,
       version: "0.1.0",
-      elixir: "~> 1.6-dev",
+      elixir: ">= 1.5.2",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls]
